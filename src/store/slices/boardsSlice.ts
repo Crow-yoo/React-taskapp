6c3df505a1e11416479438de289b2529e1ccc060
Crow-yoo <dbwltkd5984@gmail.com>
2024-11-06 16:58:46 +0900

    #3. Edit Modal, Logger, LogItem components 추가 및 게시판 삭제 기능 구현

--- src/store/slices/boardsSlice.ts
+++ src/store/slices/boardsSlice.ts
@@ -26,6 +26,16 @@ type TAddTaskAction = {
     task: ITask;
 }
 
+type TDeleteTaskAction = {
+    boardId: string;
+    listId: string;
+    taskId: string;
+}
+
+type TDeleteBoardAction = {
+    boardId: string;
+}
+
 const initialState: TBoardsState = {
     modalActive: false,
     boardArray: [
@@ -76,6 +86,12 @@ const boardSlice = createSlice({
             state.boardArray.push(payload.board);
         },
 
+        deleteBoard: (state, { payload }: PayloadAction<TDeleteBoardAction>) => {
+            state.boardArray = state.boardArray.filter(
+                board => board.boardId !== payload.boardId
+            )
+        },
+
         addList: (state, { payload }: PayloadAction<TAddListAction>) => {
             state.boardArray.map(board =>
                 board.boardId === payload.boardId
@@ -102,6 +118,54 @@ const boardSlice = createSlice({
             )
         },
 
+        updateTask: (state, { payload }: PayloadAction<TAddTaskAction>) => {
+            state.boardArray = state.boardArray.map(board =>
+                board.boardId === payload.boardId
+                ?
+                {
+                    ...board,
+                    lists: board.lists.map(list =>
+                        list.listId === payload.listId
+                        ?
+                        {
+                            ...list,
+                            tasks: list.tasks.map(task =>
+                                task.taskId === payload.task.taskId
+                                ? payload.task
+                                : task
+                            )
+                        }
+                        :
+                        list
+                    )
+                }
+                :
+                board
+            )
+        },
+
+        deleteTask: (state, { payload }: PayloadAction<TDeleteTaskAction>) => {
+            state.boardArray = state.boardArray.map(board =>
+                board.boardId === payload.boardId
+                ?
+                    {
+                        ...board,
+                        lists: board.lists.map(list =>
+                            list.listId === payload.listId
+                            ? {
+                                ...list,
+                                tasks: list.tasks.filter(
+                                    task => task.taskId !== payload.taskId
+                                )
+                            }
+                            : list
+                        )
+                    }
+                :
+                board
+            )
+        },
+
         deleteList: (state, {payload}: PayloadAction<TDeleteListAction>) => {
             state.boardArray = state.boardArray.map(
                 board =>
@@ -125,5 +189,5 @@ const boardSlice = createSlice({
 })
 
 
-export const { addBoard, deleteList, setModalActive, addTask, addList } = boardSlice.actions;
+export const { deleteBoard ,addBoard, deleteList, deleteTask, updateTask, setModalActive, addTask, addList } = boardSlice.actions;
 export const boardsReducer = boardSlice.reducer;
